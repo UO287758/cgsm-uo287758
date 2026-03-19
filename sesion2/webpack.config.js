06626e72c3724087f6dd02a7aaec2a5231ec3940
Manuel Fernández Álvarez <UO287758@uniovi.es>
2026-03-19 19:04:12 +0100

    el sol tiene shader :D

--- sesion2/webpack.config.js
+++ sesion2/webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
       "prac2-4": "./src/prac2-4.js",
       "prac2-5": "./src/prac2-5.js",
       "prac2-6": "./src/prac2-6.js",
-      "prac2-7": "./src/prac2-7.js"
+      "prac2-7": "./src/prac2-7.js",
+      "prac2-8": "./src/prac2-8.js"
     },
     output: {
         filename: '[name].js'
@@ -31,6 +32,12 @@ module.exports = {
         type: 'javascript/auto',
         resolve: {
           fullySpecified: false
+        }
+      },
+      {
+        test: /\.glsl$/,
+        use: {
+          loader: 'webpack-glsl-loader'
         }
       }
     ]
